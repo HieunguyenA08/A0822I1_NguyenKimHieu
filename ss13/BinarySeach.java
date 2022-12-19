5660a1e3df164416527a8e4657628c60edb541cb
HieunguyenA08 <112401830+HieunguyenA08@users.noreply.github.com>
2022-12-19 18:35:22 +0700

    Add files via upload

--- ss13/BinarySeach.java
+++ ss13/BinarySeach.java
@@ -1,6 +1,25 @@
 package ss13;
 
+import java.util.Arrays;
+
 public class BinarySeach {
+    public static void main(String[] args) {
+
+        BinarySeach binarySeach = new BinarySeach();
+        int [] arr = {1,7,3,9,3,5,8};
+        int temp =arr[0];
+        for (int i = 0 ; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(binarySeach.binarySearch(arr,5));
+    }
 
     public int binarySearch(int[] list, int key){
         int high = list.length-1;
